7cb1e66dc5b667aa9b1b7bd59b1f3ea1068d3f23
TajamalHussain <tajamalhussain7878@gmail.com>
2019-04-12 23:40:28 +0500

    Start Institute Development

---
+++ application/views/files/footer.php
@@ -0,0 +1,6 @@
+<script href="<?php echo base_url('assets/js/jquery.ja'); ?>"></script>
+<script>
+	// alert(0);
+</script>
+</body>
+</html>
---
+++ application/views/files/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/materialize.min.css') ?> ">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/materialize.min.js') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/stylesheet.css') ?>">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+<body>
+<!-- The video -->
+<video autoplay muted loop id="myVideo" style="position: absolute; z-index: 10">
+  <source src="https://digiskills.pk/images/videos/DigiSkills_HeaderVideo.mp4" type="video/mp4">
+</video>
+<div class="content" style="z-index: 50;background: red;">
+  <h1>Heading</h1>
+  <p>Lorem ipsum...</p>
+</div>
